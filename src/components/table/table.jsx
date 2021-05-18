3f1caa0f5e87c745dd018eae82def6c4546e025b
Ibarsuk <ivanbarsukov7@gmail.com>
2021-05-18 23:58:32 +0300

    sort

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,13 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 
 import {fetchUsers} from "../../store/api-actions";
-import {MAX_TABLE_ROWS} from '../../const';
+import {MAX_TABLE_ROWS, SortType} from '../../const';
 
 import {getIfUsersLoaded, getUsers} from "../../store/reducers/data/selectors";
 import {getTableType} from "../../store/reducers/work-process/selectors";
 
 import TableRow from '../table-row/table-row';
+
+const initialSliceData = {
+  start: 0,
+  fin: MAX_TABLE_ROWS
+};
+
+const initialSortData = {
+  sort: SortType.NONE,
+  isSortReversed: false
+};
+
+const SliceDataUpdate = {
+  NEXT: (prevData) => ({
+    start: prevData.fin,
+    fin: prevData.fin + MAX_TABLE_ROWS
+  }),
+  PREV: (prevData) => ({
+    start: prevData.start - MAX_TABLE_ROWS,
+    fin: prevData.start
+  })
+};
+
+const Sort = {
+  [SortType.NONE]: (users) => users,
+  [SortType.ID]: (users) => users.slice().sort((prev, curr) => prev.id - curr.id),
+  [SortType.FIRST_NAME]: (users) => users.slice().sort((prev, curr) => prev.firstName.localeCompare(curr.firstName)),
+  [SortType.LAST_NAME]: (users) => users.slice().sort((prev, curr) => prev.lastName.localeCompare(curr.lastName)),
+  [SortType.EMAIL]: (users) => users.slice().sort((prev, curr) => prev.email.localeCompare(curr.email))
+};
 
 const Table = () => {
   const dispatch = useDispatch();
@@ -16,11 +45,35 @@ const Table = () => {
   const usersLoaded = useSelector(getIfUsersLoaded);
   const users = useSelector(getUsers);
 
-  const [lastRow, setLastRow] = useState(MAX_TABLE_ROWS)
+  const [sliceData, setSliceData] = useState(initialSliceData);
+  const [sortInfo, setSort] = useState(initialSortData);
+  const {sort, isSortReversed} = sortInfo;
+
+  const usersToRender = useMemo(() => {
+    const sortedUsers = Sort[sort](users)
+    const usersPreparedForSlice = isSortReversed ? sortedUsers.reverse() : sortedUsers;
+    return usersPreparedForSlice.slice(sliceData.start, sliceData.fin);
+  },
+    [users, sort, sliceData, isSortReversed]
+  );
+
+  const isNextStepButtonDisabled = sliceData.fin + MAX_TABLE_ROWS > users.length;
+  const isPreviousStepButtonDisabled = sliceData.start - MAX_TABLE_ROWS < 0;
 
   useEffect(() => {
     dispatch(fetchUsers(tableType))
   }, [])
+
+  const handleTableHeadersClick = (sortType) => () => {
+    setSliceData(initialSliceData);
+
+    setSort((prevSortInfo) => ({
+      sort: sortType,
+      isSortReversed: prevSortInfo.sort === sortType ? !prevSortInfo.isSortReversed : false
+    }))
+  };
+
+  const handleAnotherPageButtonClick = (SliceDataUpdate) => () => setSliceData(SliceDataUpdate);
 
   if (!usersLoaded) {
     return <h2>LOADING</h2>;
@@ -31,17 +84,38 @@ const Table = () => {
       <table>
         <tbody>
           <tr>
-           <th>id</th>
-           <th>firstName</th>
-           <th>LastName</th>
-           <th>email</th>
+           <th onClick={handleTableHeadersClick(SortType.ID)}>id</th>
+           <th onClick={handleTableHeadersClick(SortType.FIRST_NAME)}>firstName</th>
+           <th onClick={handleTableHeadersClick(SortType.LAST_NAME)}>LastName</th>
+           <th onClick={handleTableHeadersClick(SortType.EMAIL)}>email</th>
            <th>phone</th>
           </tr>
           {
-            users.slice(0, lastRow).map((value) => <TableRow  {...value} key={`user ${value.id}`}/>)
+            usersToRender.map((value, i) => <TableRow  {...value} key={`${i}-user ${value.id}`}/>)
           }
         </tbody>
       </table>
+      {
+        users.length > MAX_TABLE_ROWS
+        &&
+        <>
+          <button
+            type="button"
+            onClick={handleAnotherPageButtonClick(SliceDataUpdate.PREV)}
+            disabled={isPreviousStepButtonDisabled}
+          >
+            Show previous
+          </button>
+
+          <button
+            type="button"
+            onClick={handleAnotherPageButtonClick(SliceDataUpdate.NEXT)}
+            disabled={isNextStepButtonDisabled}
+          >
+            Show next
+          </button>
+        </>
+      }
     </section>
   );
 };
--- src/const.js
+++ src/const.js
@@ -8,4 +8,12 @@ export const ApiRoutes = {
   [TableType.BIG]: `/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
 };
 
+export const SortType = {
+  NONE: `NONE`,
+  ID: `ID`,
+  FIRST_NAME: `FIRST_NAME`,
+  LAST_NAME: `LAST_NAME`,
+  EMAIL: `EMAIL`
+};
+
 export const MAX_TABLE_ROWS = 50;
